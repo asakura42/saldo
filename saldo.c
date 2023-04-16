43f8199022c8279a70b6fe623064918780970313
anonymous <anonymous@somemailprovider.test>
2023-04-16 15:46:27 +0300

    new option

--- saldo.c
+++ saldo.c
@@ -7,10 +7,11 @@
 // Function to print help text when --help option is detected
 void print_help_text() {
 	printf("This program calculates the current saldo for a given month by reading expenses from a configuration file.\n");
-	printf("Usage: saldo [--help|-h] [--edit|-e [day] [expense]] [--add|-a [day] [expense]]\n");
+	printf("Usage: saldo [--help|-h] [--edit|-e [day] [expense]] [--add|-a [day] [expense]] [--new-config|--rebuild-config]\n");
 	printf("--help|-h: Displays the help text.\n");
 	printf("--edit|-e [day] [expense]: Edits the expenses for the specified day.\n");
 	printf("--add|-a [day] [expense]: Adds the specified expense to the existing expense for the specified day.\n");
+	printf("--new-config|--rebuild-config: rebuilds config (for example, after starting next month). Be careful!\n")
 	printf("If the configuration file is not found, the program will generate one in ~/.local/share/saldo_config.txt.\nThe configuration file contains the income, fixed expenses and expenses for each day of the month.\nThe program will then calculate the saldo for each day based on the daily budget (income - fixed expenses / days in the month).\nThe daily budget and the saldo for each day will be displayed.\n");
 	exit(0);
 }
@@ -22,9 +23,35 @@ int main(int argc, char *argv[]) {
 	sprintf(configFilePath, "%s/.local/share/saldo_config.txt", getenv("HOME"));
 	FILE *configFile = fopen(configFilePath, "r+"); // Read and write
 
+	// Get the current month
+	time_t rawtime;
+	struct tm *timeinfo;
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+	month = timeinfo->tm_mon + 1;
+
+	// Get the number of days in the month
+	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+		daysInMonth = 31;
+	else if (month == 2)
+		daysInMonth = 28;
+	else
+		daysInMonth = 30;
+
 	// Print help text if --help option is detected
 	if (argc > 1 && (strcmp(argv[1],"--help") == 0 || strcmp(argv[1],"-h") == 0)) {
 		print_help_text();
+	}
+
+	if (argc > 1 && (strcmp(argv[1],"--new-config") == 0 || strcmp(argv[1],"--rebuild-config") == 0)) {
+		printf("Rebuilding config...\nDon't forget to edit it\n");
+		FILE *configFile = fopen(configFilePath, "w");
+		fprintf(configFile, "income 0\nfixed_expenses 0\n");
+		for (int i = 1; i <= daysInMonth; i++) {
+			fprintf(configFile, "%d 0\n", i);
+		}
+		fclose(configFile);
+		exit(0);
 	}
 
 
@@ -90,22 +117,6 @@ int main(int argc, char *argv[]) {
 
 		exit(0);
 	}
-
-
-	// Get the current month
-	time_t rawtime;
-	struct tm *timeinfo;
-	time(&rawtime);
-	timeinfo = localtime(&rawtime);
-	month = timeinfo->tm_mon + 1;
-
-	// Get the number of days in the month
-	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
-		daysInMonth = 31;
-	else if (month == 2)
-		daysInMonth = 28;
-	else
-		daysInMonth = 30;
 
 	// Check if the configuration file exists and generate it if not
 	if (!configFile) {
